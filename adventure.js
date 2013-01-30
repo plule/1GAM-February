f82d79398aa347fb03c52d284022e8311df970bb
Pierre Lulé <pierre@lule.fr>
2013-01-30 22:28:49 +0100

    basic parsing

--- adventure.js
+++ adventure.js
@@ -1,22 +1,23 @@
-adventure = {
-    nodes = {
-        roomNode = {
-            text = "This [text|textLink] is a link. [This one|textBis] too and can be transformed",
-            properties = {
-                textLink = {
-                    clickDestination = "hallNode"
+Adventure = {
+    nodes: {
+        roomNode: {
+            text: "[text|textLink][textagain|ya] is a link. [This one|textBis] too and can be transformed",
+            properties: {
+                textLink: {
+                    clickDestination: "hallNode"
                 },
-                textBis = {
-                    clickDestination = "hallNode",
-                    waveText = "Hidden text"
-                }
-            },
-            hallNode = {
-                text = "This is a simple node without link. But it has an [obfuscated|obfText] text.",
-                properties = {
-                    obfText = {obfuscated = true}
+                textBis: {
+                    clickDestination: "hallNode",
+                    waveText: "Hidden text"
                 }
             }
+		},
+        hallNode: {
+            text: "This is a simple node without link. But it has an [obfuscated|obfText] text.",
+            properties: {
+                obfText: {obfuscated: true}
+            }
         }
-    }
+    },
+	startNode: 'roomNode'
 }
--- engine.js
+++ engine.js
@@ -1,1 +1,34 @@
-$('#adventure').append('<p>it works</p>');
+var parseText = function(text) {
+	var output = $(document.createElement('div'));
+	var linkExp =  /\[([^\[\]\|]+)\|(\w+)\]/g;
+	
+	var lastIndex = 0;
+	while((regArray = linkExp.exec(text)) !== null) {
+		
+		var rawText = text.substring(lastIndex, regArray.index);
+		if(rawText) {
+			output.append($('<span>')
+						  .append(rawText)
+						  .addClass('raw-text'))
+		}
+		
+		output.append($('<span>')
+					  .append(regArray[1])
+					  .addClass('format-text'));
+
+		lastIndex = linkExp.lastIndex;
+	}
+	var rawText = text.substring(lastIndex);
+	if(rawText) {
+		output.append($('<span>')
+					  .append(rawText)
+					  .addClass('raw-text'));
+	}
+
+	return output;
+}
+
+var CurrentNode = Adventure.nodes[Adventure.startNode];
+
+var output = parseText(CurrentNode.text);
+$('#adventure').append(output);//adventure.nodes.roomNode.text);
